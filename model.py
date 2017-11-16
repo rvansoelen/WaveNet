fd991aa85d4f58a1ad57659821f6e5a4ee4c1160
rvansoelen <vansoelen.1@osu.edu>
2017-11-15 22:56:59 -0500

    Added code for creating the wavenet model. StNot yet complete

--- model.py
+++ model.py
@@ -1,20 +1,61 @@
 #This is where the wavenet model will be defined
-
+import tensorflow as tf
 
 class WaveNet:
+	'''Implements the WaveNet network for generative audio.
+    
+    Usage (with the architecture as in the DeepMind paper):
+        dilations = [2**i for i in range(N)] * M
+        filter_width = 2  # Convolutions just use 2 samples.
+        residual_channels = 16  # Not specified in the paper.
+        dilation_channels = 32  # Not specified in the paper.
+        skip_channels = 16      # Not specified in the paper.
+        net = WaveNetModel(batch_size, dilations, filter_width,
+                           residual_channels, dilation_channels,
+                           skip_channels)
+        loss = net.loss(input_batch)
+    '''
+
 	#initialize class
 	def __init__(self):
+		#TODO: Fill these variables
+		self.dilations = None
 
 
 	#create and return model
-	def createModel(self):
+	def createModel(self, input):
+		#TODO: try not using currentLayer variable to make it more clear where each layer is defined
+		currentLayer = input
+
 		#preprocessing layer
 
 		#causal layer
-
+		currentLayer = self.createCausalLayer(currentLayer) #"causalLayer" named scope is defined here?
+		
 		#dilated convolutional stack 
+		outputs = []
+		with tf.named_scope('Dilated Stack'):
+			for layerNumber, dilation in enumerate(self.dilations):
+				with tf.named_scope('Dilated Layer '+str(layerNumber)):
+					output, currentLayer = self.createDilatedLayer(currentLayer)
+					outputs.append(output)
 
 		#post processing layer
+		with tf.named_scope('Post-Processing'):
+			w1 = createVariable('postWeights1', shapeW1)
+			b1 = createVariable('postBias1', shapeB1)
+			w2 = createVariable('postWeights1', shapeW2)
+			b2 = createVariable('postBias1', shapeB2)
+
+			total = tf.nn.relu(sum(ouputs))
+			conv1 = tf.nn.conv1d(transformed1, w1, stride=1, padding="SAME")
+     		conv1 = tf.nn.relu(tf.add(conv1, b1))
+
+     		conv2 = tf.nn.conv1d(transformed2, w2, stride=1, padding="SAME")
+            conv2 = tf.add(conv2, b2)
+            return conv2
+
+
 
 		#loss layer
 
