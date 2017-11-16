632934ea914ac07752e6eff1b611c4ae63595e5f
rvansoelen <vansoelen.1@osu.edu>
2017-11-15 20:06:05 -0500

    Added file for extracting needed data from the YouTube M8 dataset

---
+++ extractData.py
@@ -0,0 +1,12 @@
+#This file extracts the data of importance from the YouTube M8 dataset
+
+
+#open the dataset files
+
+#keep only videos with the specified labels (piano, for example)
+
+#get the audio
+
+#possibly downsample audio
+
+#save it as a new tensorflow record file
