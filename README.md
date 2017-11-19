df5105d0cfa85291253452131a085e8b6ab41974
Ryan <Ryan Van Soelen>
2017-11-19 02:10:50 -0500

    Added more code to model.py. More work needs to be done

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A tensorflow implementation of DeepMind's WaveNet
 
 Data was obtained from the YouTube 8M dataset. The dataset is partitioned into {frame_level,video_level} and {train,validate,test}. Only frame level data was used.
 
-To obtain the dataset, do:
-  curl data.yt8m.org/download.py | partition=1/video_level/train mirror=us python
+To obtain the dataset, do (Requires 1.7 TB of free space):
+  curl data.yt8m.org/download.py | partition=1/frame_level/train mirror=us python
 
 Or to download just 1/1000th of the data:
-  curl data.yt8m.org/download.py | shard=1,1000 partition=1/video_level/train mirror=us python
+  curl data.yt8m.org/download.py | shard=1,1000 partition=1/frame_level/train mirror=us python
--- model.py
+++ model.py
@@ -22,6 +22,20 @@ class WaveNet:
 		self.dilations = None
 
 
+	def createCausalLayer(self):
+
+
+	def createDilatedLayer(self, input, name='Dilated Layer'):
+		with tf.variable_scope(name):
+			w = tf.createVariable('weights')
+			b = tf.createVariable('bias')
+			conv = tf.conv1d(input, w, stride=1, padding='SAME')
+			conv = tf.add(conv, b)
+			actv = tf.tanh(conv)*tf.sigmoid(conv)
+			skip = actv
+			residual = tf.add(actv, input)
+			return skip, residual
+
 	#create and return model
 	def createModel(self, input):
 		#TODO: try not using currentLayer variable to make it more clear where each layer is defined
@@ -34,32 +48,32 @@ class WaveNet:
 		
 		#dilated convolutional stack 
 		outputs = []
-		with tf.named_scope('Dilated Stack'):
+		with tf.variable_scope('Dilated Stack'):
 			for layerNumber, dilation in enumerate(self.dilations):
 				with tf.named_scope('Dilated Layer '+str(layerNumber)):
-					output, currentLayer = self.createDilatedLayer(currentLayer)
-					outputs.append(output)
+					#represents skip(direct connection to last layer) and residual(normal) connections
+					skipOutput, currentLayer = self.createDilatedLayer(currentLayer)
+					outputs.append(skipOutput)
 
 		#post processing layer
-		with tf.named_scope('Post-Processing'):
+		with tf.variable_scope('Post-Processing'):
 			w1 = createVariable('postWeights1', shapeW1)
 			b1 = createVariable('postBias1', shapeB1)
 			w2 = createVariable('postWeights1', shapeW2)
 			b2 = createVariable('postBias1', shapeB2)
 
-			total = tf.nn.relu(sum(ouputs))
+			total = tf.nn.relu(sum(outputs))
 			conv1 = tf.nn.conv1d(transformed1, w1, stride=1, padding="SAME")
      		conv1 = tf.nn.relu(tf.add(conv1, b1))
 
      		conv2 = tf.nn.conv1d(transformed2, w2, stride=1, padding="SAME")
             conv2 = tf.add(conv2, b2)
-            return conv2
+        return conv2
 
-
-
-		#loss layer
-
-		#optimizer
-
-		#return model
+    def wrapLossOptimizer(self, model):
+		with tf.named_scope('Loss'):
+			#loss layer
+			target = tf.placeholder(tf.float)
+			loss = tf.softmax_cross_entropy_with_logits(logits=model, labels=target)
+		return loss
 
